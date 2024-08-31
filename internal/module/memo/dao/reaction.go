33fe0e8c07853cf7d66599de50f4af6bec9908aa
yearnfar <yearnfar@gmail.com>
2024-09-01 00:09:33 +0800

    feat: dao

--- internal/module/memo/dao/reaction.go
+++ internal/module/memo/dao/reaction.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/yearnfar/memos/internal/module/memo/model"
 	"github.com/yearnfar/memos/internal/pkg/db"
+	"gorm.io/gorm"
 )
 
 func (dao *Dao) FindReactions(ctx context.Context, req *model.FindReactionsRequest) (list []*model.Reaction, err error) {
@@ -21,7 +22,23 @@ func (dao *Dao) FindReactions(ctx context.Context, req *model.FindReactionsReque
 	err = conn.Find(&list).Error
 	return
 }
+
+func (dao *Dao) FindReaction(ctx context.Context, req *model.FindReactionsRequest) (*model.Reaction, error) {
+	list, err := dao.FindReactions(ctx, req)
+	if err != nil {
+		return nil, err
+	} else if len(list) == 0 {
+		return nil, gorm.ErrRecordNotFound
+	}
+	return list[0], nil
+}
+
 func (dao *Dao) CreateReaction(ctx context.Context, m *model.Reaction) (err error) {
 	err = db.GetDB(ctx).Create(&m).Error
 	return
+}
+
+func (dao *Dao) DeleteReaction(ctx context.Context, id int32) error {
+	err := db.GetDB(ctx).Model(&model.Reaction{}).Delete("id=?", id).Error
+	return err
 }
